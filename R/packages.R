75b878b413c11e9aebafc59fbe6a0f2ffff0106f
Sharla Gelfand <sharla.gelfand@gmail.com>
2023-09-12 09:48:40 -0400

    Remove function

--- R/packages.R
+++ R/packages.R
@@ -28,40 +28,6 @@ list_packages <- function(limit = 50, token = get_token()) {
   complete_package_res(packages)
 }
 
-#' Search packages by title
-#'
-#' Search portal packages by title.
-#'
-#' @param title Title to search (case-insensitive).
-#' @param limit Maximum number of packages to return. The default is 50. The maximum limit is 1000.
-#' @inheritParams list_packages
-#'
-#' @export
-#'
-#' @return A tibble of matching packages along with package metadata, including \code{title}, \code{id}, \code{topics}, \code{civic_issues}, \code{excerpt}, \code{publisher}, \code{dataset_category}, \code{num_resources} (the number of resources in the package), \code{formats} (the different formats of the resources), \code{refresh_rate} (how often the package is refreshed), and \code{last_refreshed} (the date it was last refreshed).
-#'
-#' @examples
-#' \donttest{
-#' search_packages("ttc")
-#' }
-search_packages <- function(title, limit = 50, token = get_token()) {
-  # TODO not working
-  check_internet()
-  packages <- ckanr::package_search(
-    fq = paste0("title:", '"', title, '"'),
-    rows = limit,
-    url = lemr_ckan_url,
-    as = "table",
-    key = token
-  )
-
-  if (length(packages[["results"]]) == 0) {
-    package_res_init
-  } else {
-    complete_package_res(packages[["results"]])
-  }
-}
-
 package_res_init <- tibble::tibble(
   title = character(),
   id = character(),
