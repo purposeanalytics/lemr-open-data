3201e3b577f0de43b80f1533b8172542b8c1acdf
Sharla Gelfand <sharla.gelfand@gmail.com>
2024-03-06 14:28:48 -0500

    R cmd check fixes

--- R/token.R
+++ R/token.R
@@ -5,24 +5,12 @@
 #' @param scope Edit globally for the current user, or locally for the current project
 #' @param var Environment variable to store token in, defaults to \code{LEMROPENDATA_KEY}
 #'
-#' @export
-#'
-#' @rdname token
-#'
-#' @examples
-#' set_token()
-#' • Modify '.Renviron'
-#' • Restart R for changes to take effect
-#' ℹ Set LEMROPENDATA_KEY to LEMR Open Data token in the opened .Renviron
-#'
-#' get_token()
-#' # [1] "test"
+#' @noRd
 set_token <- function(scope = c("project", "user"), var = "LEMROPENDATA_KEY") {
   usethis::edit_r_environ(scope)
-  usethis::ui_info("Set {var} to LEMR Open Data token in the opened .Renviron")
+  usethis::ui_info("Set {var} to LEMR Housing Monitor open data portal token in the opened .Renviron")
 }
 
-#' @rdname token
 get_token <- function(var = "LEMROPENDATA_KEY") {
   Sys.getenv(var)
 }
